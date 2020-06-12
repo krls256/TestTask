1526c64589fb4df21d279b6b2c0b281d160726e0
Antiselekotin <deksazon8@gmail.com>
2020-06-12 17:36:25 +0300

    berofe ariving

---
+++ src/components/app/app.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Menu from '../menu'
+import './app.scss'
+
+const App = () => {
+    return (
+        <div className="app">
+            <Menu/>
+        </div>
+    )
+}
+
+export default App;
---
+++ src/components/menu/menu.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import 'menu.scss';
+
+const Menu = () => {
+    return (
+        <div className='menu'>
+            <div className=''></div>
+        </div>
+    )
+}
+
+export default Menu;
